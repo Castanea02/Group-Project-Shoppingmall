051b6d954f1f057894ba3646296f797d4dd035f5
justin951222 <justin951222@naver.com>
2024-02-01 22:50:35 +0900

    UserEdit Complete

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const App = () => {
             <Login />
           </Route>
           <Route path="/editCheck">
-            <AuthEditCheck />
+            <AuthEditCheck loggedIn={loggedIn} />
           </Route>
           <Route path="/edit">
             <AuthEdit />
--- src/Routes/Edit.tsx
+++ src/Routes/Edit.tsx
@@ -1,54 +1,100 @@
 import { Container, Center, Input, Button, useToast } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useSetRecoilState } from "recoil";
 import { useMutation } from "react-query";
 import { useHistory } from "react-router-dom";
+import { useEditMutation } from "../api";
 import { isUserAtom } from "../atoms";
 import { useEditCheckMutation } from "../api";
 import { ErrorLabel, Label, Title } from "../components/FormLabel";
 import NotFound from "../components/NotFound";
 import Auth from "../components/Auth";
 
+interface IEditForm {
+  pw: string;
+  confPw: string;
+  email: string;
+}
 /**개인정보 수정  */
 function Edit(props: any) {
   const isLogin = useSetRecoilState(isUserAtom);
-  const mutation = useMutation(useEditCheckMutation);
+  const mutation = useMutation(useEditMutation);
   const history = useHistory();
   const toast = useToast();
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IEditForm>();
 
-  const onSubmit = async (formData: any) => {
+  const logout = () => {
+    fetch(`${process.env.REACT_APP_NODE_ADDRESS}/api/logout`, {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        isLogin(false);
+      });
+  };
+
+  const onSubmit: SubmitHandler<IEditForm> = async (formData: any) => {
     try {
+      if (!(formData.pw || formData.confPw || formData.email)) {
+        toast({
+          position: "top",
+          title: "실패",
+          description: "수정할 정보를 입력하세요",
+          status: "error",
+          duration: 3000,
+          isClosable: false,
+        });
+        return;
+        //비번이 들어왔지만 비번이 일치하지 않을 경우
+      } else if (
+        (formData.pw || formData.confPw) &&
+        formData.pw != formData.confPw
+      ) {
+        toast({
+          position: "top",
+          title: "실패",
+          description: "비밀번호와 비밀번호 확인이 일치하지 않습니다.",
+          status: "error",
+          duration: 3000,
+          isClosable: false,
+        });
+        return;
+      }
       // useMutation 훅을 호출하여 로그인 비동기 작업 실행
       const data = await mutation.mutateAsync(formData);
+      console.log(data);
       if (data.success) {
         isLogin(true);
         toast({
           position: "top",
           title: `성공`,
-          description: `수정할 정보를 입력하세요`,
+          description: `정보가 수정되었습니다 로그아웃 되었습니다 다시 로그인 해주시길 바랍니다.`,
           status: "success",
           duration: 3000,
           isClosable: false,
         });
-        history.push("/edit");
+        logout();
+        history.push("/");
       } else {
         toast({
           position: "top",
           title: "실패",
-          description:
-            "비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.",
+          description: "변경하실 정보가 이전 정보와 같습니다.",
           status: "error",
           duration: 3000,
           isClosable: false,
         });
       }
     } catch (error) {
-      console.error("로그인 실패:", error);
+      console.error("정보 변경 실패:", error);
     }
   };
   return (
@@ -57,44 +103,29 @@ function Edit(props: any) {
       borderColor="black.200"
       borderRadius="5"
       mt={40}
-      padding={10}>
-      {props.loggedIn ? (
-        <>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Center>
-              <Title>개인정보수정</Title>
-            </Center>
-            <Label>Password</Label>
-            <Input
-              {...register("pw", { required: true })}
-              type="password"
-              placeholder="Password"
-            />
-            {errors.pw && <ErrorLabel>비밀번호를 입력해주세요</ErrorLabel>}
-            <Label>Confirm Password</Label>
-            <Input
-              {...register("confPw", { required: true })}
-              type="password"
-              placeholder="Confirm Password"
-            />
-            {errors.pw && <ErrorLabel>확인 비밀번호를 입력해주세요</ErrorLabel>}
-            <Label>Email</Label>
-            <Input
-              {...register("email", { required: true })}
-              placeholder="Email"
-            />
-            {errors.email && <ErrorLabel>이메일을 입력해주세요</ErrorLabel>}
-            <Button type="submit" mt={2} colorScheme="purple">
-              Submit
-            </Button>
-          </form>
-          <a href="/">뒤로</a>{" "}
-        </>
-      ) : (
-        <NotFound />
-      )}
+      padding={10}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Center>
+          <Title>개인정보수정</Title>
+        </Center>
+        <Label>Password</Label>
+        <Input {...register("pw")} type="password" placeholder="Password" />
+        <Label>Confirm Password</Label>
+        <Input
+          {...register("confPw")}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <Label>Email</Label>
+        <Input {...register("email")} placeholder="Email" />
+        <Button type="submit" mt={2} colorScheme="purple">
+          Submit
+        </Button>
+      </form>
+      <a href="/">뒤로</a>
     </Container>
   );
 }
-const AuthEdit = Auth(Edit);
-export default AuthEdit;
+
+export default Edit;
